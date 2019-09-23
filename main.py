759c58ec195cbbeb9e5e7b5edc6f69fc6be893e4
Chanaka Rathnayaka <bmcrathnayaka@gmail.com>
2019-09-23 18:29:28 +0530

    functions separated to files

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python2.7
 import sys
-import socket
 import time
+from DestinationValidator import isDestinationPortOpen
 
 ip = "161.47.68.144"
 port = 2112
@@ -9,19 +9,11 @@ retry = 1
 delay = 1
 timeout = 2
 
-def isOpen(ip, port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        s.connect((ip, int(port)))
-        s.shutdown(2)
-        return True
-    except:
-        return False
 
 def checkHost(ip, port):
     ipup = False
     for i in range(retry):
-        if isOpen(ip, port):
+        if isDestinationPortOpen(ip, port):
             ipup = True
             break
         else:
